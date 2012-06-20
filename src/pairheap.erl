85bcbc8abc388addd8f5a109f6fcf182fff47916
refefer <astanton@theatlantic.com>
2012-06-20 09:03:56 -0400

    moves to proper src directory and optimizes pairheap for memory usage.

--- src/pairheap.erl
+++ src/pairheap.erl
@@ -15,7 +15,7 @@ find_min({Elem,_SubHeap}) ->
     {ok, Elem}.
     
 insert(Heap, Data) ->
-    merge(Heap, {Data, empty}).
+    merge(Heap, {Data}).
 
 from_list(Items) ->
     lists:foldl(fun(Item, Heap) -> insert(Heap, Item) end, new(), Items).
@@ -24,20 +24,29 @@ merge(Heap1, empty) ->
     Heap1;
 merge(empty, Heap2) ->
     Heap2;
-merge({E1, SH1}, {E2, SH2}) ->
+merge(Heap1, Heap2) ->
+    E1 = first(Heap1),
+    E2 = first(Heap2),
     if
         E1 < E2 ->
-            {E1, [{E2, SH2} | to_list(SH1)]};
+            {E1, [Heap2 | second(Heap1)]};
         true ->
-            {E2, [{E1, SH1} | to_list(SH2)]}
+            {E2, [Heap1 | second(Heap2)]}
     end.
 
-to_list(empty) -> [];
-to_list(L) -> L.
+first({E1, SH1}) ->
+    E1;
+first(E1) ->
+    E1.
+
+second({E1, SH1}) ->
+    SH1;
+second(E1) ->
+    [].
 
 delete_min([]) ->
     {error, empty};
-delete_min({_Elem, empty}) ->
+delete_min({Elem}) ->
     {ok, empty};
 delete_min({_Elem, SubHeaps}) ->
     {ok, merge_pairs(SubHeaps)}.
